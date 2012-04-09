8d10fa8b083c95ffc177a5bf2f73adf525ff40c5
Chris Howlett <chowlett09+github@gmail.com>
2012-04-09 11:45:41 +0100

    Add substepping to Island for PBEM; fixes #15

--- app/models/seaside/island.rb
+++ app/models/seaside/island.rb
@@ -41,13 +41,14 @@ class Seaside::Island < Card
                           :name => "setaside",
                           :text => "Set Aside",
                           :nil_action => nil,
-                          :params => {:card => "#{self.class}#{id}"},
+                          :params => {:card => "#{self.class}#{id}",
+                                      :substep => "setaside"},
                           :cards => [true] * player.cards.hand.size
                          }]
     end
   end
   
-  def resolve(ply, params, parent_act)
+  def resolve_setaside(ply, params, parent_act)
     # We expect to have been passed a :card_index
     if (not params.include? :nil_action) and (not params.include? :card_index)
       return "Invalid parameters"
