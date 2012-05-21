8cba24a69000f2f9f00d1aafa6cedb378f03fb39
Chris Howlett <chowlett09+github@gmail.com>
2012-05-21 14:17:54 +0100

    Player not in scope after trash. Fixes #5

--- app/models/seaside/treasure_map.rb
+++ app/models/seaside/treasure_map.rb
@@ -8,28 +8,31 @@ class Seaside::TreasureMap < Card
   def play(parent_act)
     super
 
+    # We're about to trash this card, which will remove the reference to player; so take a local copy
+    ply = player
+
     # Trash this card, and look for another Tresaure Map in hand
     trash
-    other = player.cards.hand(true).of_type("Seaside::TreasureMap")[0]
+    other = ply.cards.hand(true).of_type("Seaside::TreasureMap")[0]
     
     if other
       # Found a second treasure map
-      game.histories.create!(:event => "#{player.name} trashed two Treasure Maps from hand.",
-                            :css_class => "player#{player.seat} card_trash")
+      game.histories.create!(:event => "#{ply.name} trashed two Treasure Maps from hand.",
+                            :css_class => "player#{ply.seat} card_trash")
       other.trash
 
       # And acquire the FOUR gold to top of deck.
       gold_pile = game.piles.find_by_card_type("BasicCards::Gold")
       4.times do
-        player.queue(parent_act, :gain, :pile => gold_pile.id, :location => "deck")
+        ply.queue(parent_act, :gain, :pile => gold_pile.id, :location => "deck")
       end
       
-      game.histories.create!(:event => "#{player.name} gained four Gold to top of their deck.", 
-                            :css_class => "player#{player.seat} card_gain")
+      game.histories.create!(:event => "#{ply.name} gained four Gold to top of their deck.", 
+                            :css_class => "player#{ply.seat} card_gain")
     else
       # No other treasure map exists.
-      game.histories.create!(:event => "#{player.name} trashed only one treasure map.",
-                            :css_class => "player#{player.seat} card_trash")
+      game.histories.create!(:event => "#{ply.name} trashed only one #{readable_name}.",
+                            :css_class => "player#{ply.seat} card_trash")
     end
 
     return "OK"
