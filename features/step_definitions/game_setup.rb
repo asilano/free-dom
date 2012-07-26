d7e5bb4f5913bc18f1599234b245b85227f272bf
Chris Howlett <chowlett09+github@gmail.com>
2012-07-26 13:36:28 +0100

    Test Trade Route

--- features/step_definitions/game_setup.rb
+++ features/step_definitions/game_setup.rb
@@ -1,7 +1,7 @@
-When /the game fact "(.*)" is (.*)/ do |fact, value|
+Given /the game fact "(.*)" is (.*)/ do |fact, value|
   @game.facts_will_change!
   value = value.to_i if value =~ /^\d+$/
   value = nil if value == "nil"
-  @game.facts[fact.to_sym] = value
+  @game.facts[fact.gsub(/\s/, '_').to_sym] = value
   @game.save
 end
--- features/step_definitions/pile_checks.rb
+++ features/step_definitions/pile_checks.rb
@@ -2,3 +2,13 @@ Then /the #{SingleCard} pile should cost (\d+)/ do |kind, cost|
   pile = @game.piles.find(:first, :conditions => {:card_type => CARD_TYPES[kind]})
   assert_equal cost.to_i, pile.cost
 end
+
+Then /the "(.*)" state of the #{SingleCard} pile should be (.*)/ do |key, kind, value|
+  pile = @game.piles.find(:first, :conditions => {:card_type => CARD_TYPES[kind]})
+  assert_equal value, pile.reload.state[key.gsub(/\s/, '_').to_sym].to_s
+end
+
+Then /the #{SingleCard} pile should have no "(.*)" state/ do |kind, key|
+  pile = @game.piles.find(:first, :conditions => {:card_type => CARD_TYPES[kind]})
+  assert_not_include pile.reload.state, key.gsub(/\s/, '_').to_sym
+end
