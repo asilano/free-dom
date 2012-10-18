04311ca98adf7760b50a99ae339a3a0e198f8413
Chris Howlett <chowlett09+github@gmail.com>
2012-10-18 13:20:19 +0100

    Further fix form attr support test

--- app/assets/javascripts/form_attr_shim.js
+++ app/assets/javascripts/form_attr_shim.js
@@ -1,15 +1,22 @@
 var supportForm = function()
 {
-  var input = document.createElement("input");
-  if ("form" in input)
-  {
-    input.setAttribute("form", "12345");
+  var input = document.createElement('input'),
+         form = document.createElement('form'),
+         formId = 'test-input-form-reference-support';
+    // Id of the remote form
+    form.id = formId;
+    // Add form and input to DOM
+    $('body').append($(form));
+    $('body').append($(input));
+    // Add reference of form to input
+    input.setAttribute('form', formId);
+    // Check if reference exists
+    var res = !(input.form == null);
+    // Remove elements
+    $(form).remove();
+    $(input).remove();
+    return res;
 
-    if (input.getAttribute("form") == "12345")
-      return true;
-  }
-
-  return false;
 }
 
 if (!supportForm())
