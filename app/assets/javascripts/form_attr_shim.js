6c2a2d774a983cccfb56f754fabe77330a46f7b4
Chris Howlett <chowlett09+github@gmail.com>
2012-10-18 13:36:45 +0100

    Further tweak shim; re-put call in external file

--- app/assets/javascripts/form_attr_shim.js
+++ app/assets/javascripts/form_attr_shim.js
@@ -18,3 +18,21 @@ var supportForm = function()
     return res;
 
 };
+
+$(function()
+{
+  if (!supportForm())
+  {
+
+    $(document).on('click', 'input[type=submit][form], button[form]', function() {
+        var form = $('#' + $(this).attr('form'));
+        form.submit();
+      })
+      .on('submit', 'form', function() {
+        var form = $(this);
+        $('input[form='+form.attr('id')+'][type!=submit]').each(function() {
+          form.append('<input type="hidden" />').attr('name', $(this).attr('name')).val($(this).val());
+        });
+      });
+  }
+});
