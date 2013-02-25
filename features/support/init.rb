f71d3d3e9da5f1f4d9984a973f020892678a8720
Chris Howlett <chowlett09+github@gmail.com>
2013-02-25 14:34:30 +0000

    Fixup cucumber require path

--- features/support/init.rb
+++ features/support/init.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__)) + './card_types'
+require File.expand_path(File.dirname(__FILE__)) + '/card_types'
 
 CARD_NAMES = CARD_TYPES.keys
 SingleCardNoCapture = /#{CARD_NAMES.join('|')}/
