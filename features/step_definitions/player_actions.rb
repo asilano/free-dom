cdc2a0224d91f3491e36ef45936a090d6e4becc3
Chris Howlett <chowlett09+github@gmail.com>
2013-03-20 13:48:20 +0000

    Make test framework use new format of Player#do_gain

--- features/step_definitions/player_actions.rb
+++ features/step_definitions/player_actions.rb
@@ -6,7 +6,7 @@ When(/^(\w*?) gain(?:s)? (.*)/) do |name, kinds|
 
   kinds.split(/,\s+/).each do |kind|
     params = {}
-    params[:pile] = @game.piles.where(:card_type => CARD_TYPES[kind].to_s)[0].id
+    params[:pile_id] = @game.piles.where(:card_type => CARD_TYPES[kind].to_s)[0].id
     pa = @game.pending_actions.where(:parent_id => nil)[0]
     params[:parent_act] = pa.id
     @players[name].do_gain(params)
