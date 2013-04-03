0af7b4c71fc074b008c5c26f042349666e79da96
Chris Howlett <chowlett09+github@gmail.com>
2013-04-03 13:43:21 +0100

    Terminology alignment [no ci]

--- app/models/hinterlands/noble_brigand.rb
+++ app/models/hinterlands/noble_brigand.rb
@@ -1,7 +1,7 @@
 class Hinterlands::NobleBrigand < Card
   action :attack => true
   costs 4
-  card_text "+1 Coin / When you buy this or play it, each other player reveals the top 2 cards of his deck, " +
+  card_text "+1 Cash / When you buy this or play it, each other player reveals the top 2 cards of his deck, " +
             "trashes a revealed Silver or Gold you choose, and discards the rest. " +
             "If he didn't reveal a Treasure, he gains a Copper. You gain the trashed cards."
 
