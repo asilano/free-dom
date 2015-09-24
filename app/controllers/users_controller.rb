8f8c66aeeb5096ca871ddebd5fd8324cf98963e4
Chris Howlett <chowlett09@gmail.com>
2015-09-24 11:49:03 +0100

    Change to strong parameters
    For #124

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,30 @@
 class UsersController < ApplicationController
   before_filter :ensure_settings, :only => [:show, :edit]
   before_filter :find_session
-  
+
   # GET /users
   # GET /users.xml
   def index
-    @users = User.find(:all, :order => :name)
+    @users = User.order(:name)
     @title = "Rankings"
-    
+
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @users }
     end
   end
-  
+
   # GET /users/1
   # GET /users/1.xml
   def show
     @user = User.find(params[:id])
-    
+
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @user }
     end
   end
-  
+
   # GET /users/new
   # GET /users/new.xml
   def new
@@ -34,39 +34,39 @@ class UsersController < ApplicationController
     else
       @user = User.new
       @title = "Register new user"
-      
+
       respond_to do |format|
         format.html # new.html.erb
         format.xml  { render :xml => @user }
       end
-    end    
+    end
   end
-  
+
   # GET /users/1/edit
-  def edit        
+  def edit
     @user = User.find(session[:user_id]) unless session[:user_id].nil?
     @title = "Preferences"
-    
+
     if @user.nil?
       flash[:warning] = "Please log in"
       redirect_to games_path
     end
   end
-  
+
   # POST /users
   # POST /users.xml
   def create
-    @user = User.new(params[:user])
-    
-    if @user.save        
+    @user = User.new(allow_params)
+
+    if @user.save
       session[:user_id] = @user.reload.id
       cookie_login if params[:remember_me]
       @user.create_ranking
-      
+
       UserMailer.registered(@user).deliver
       UserMailer.report_registered(@user).deliver
-      
-      redirect_to registered_path               
+
+      redirect_to registered_path
     else
       respond_to do |format|
         format.html { render :action => "new" }
@@ -74,7 +74,7 @@ class UsersController < ApplicationController
       end
     end
   end
-  
+
   def registered
     @title = "Resigtration successful"
     respond_to do |format|
@@ -82,17 +82,17 @@ class UsersController < ApplicationController
       format.xml  { render :xml => @user, :status => :created, :location => @user }
     end
   end
-  
+
   # PUT /users/1
   # PUT /users/1.xml
   def update
     @user = User.find(params[:id])
-    
+
     respond_to do |format|
       if params[:id].to_i != session[:user_id]
         flash[:warning] = "Failed to update user #{@user.name} - not logged in as that user"
         format.html { redirect_to(:controller => :games, :action => :index) }
-      elsif @user.update_attributes(params[:user])
+      elsif @user.update_attributes(allow_params)
         session[:user_id] = @user.id
         flash[:notice] = "Settings updated for #{@user.name}"
         format.html { redirect_to(:controller => :users, :action => "edit", :method => :get) }
@@ -103,19 +103,19 @@ class UsersController < ApplicationController
       end
     end
   end
-  
+
   # DELETE /users/1
   # DELETE /users/1.xml
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    
+
     respond_to do |format|
       format.html { redirect_to(users_url) }
       format.xml  { head :ok }
     end
   end
-  
+
   # Handle login
   def login
     if request.post?
@@ -138,15 +138,15 @@ class UsersController < ApplicationController
         params[:name] = flash[:name]
         @title = "Login"
       end
-    end      
+    end
   end
-  
+
   def logout
     uncookie_login
     session[:user_id] = nil
     redirect_to(games_path)
-  end  
-  
+  end
+
   # Handle a forgotten password
   def password_reset
     if request.post?
@@ -155,28 +155,37 @@ class UsersController < ApplicationController
         new_pass = @user.reset_password
         UserMailer.password_reset(@user, new_pass).deliver
         flash[:notice] = "A new password has been sent to the email address on record for #{@user.name}"
-        flash[:name] = params[:name] 
-        redirect_to login_path        
+        flash[:name] = params[:name]
+        redirect_to login_path
       end
     else
       @title = "Reset password"
     end
   end
-  
+
 private
-  
+
+  def allow_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :contact_me, :pbem,
+                                  :settings_attributes => [:automoat, :autocrat_victory, :update_interval,
+                                                            :autobaron, :autotorture_curse, :automountebank,
+                                                            :autotreasury, :autoduchess, :autofoolsgold,
+                                                            :autooracle, :autoscheme, :autotunnel,
+                                                            :autobrigand, :autoigg])
+  end
+
   def ensure_settings
     if params[:id]
       user = User.find(params[:id])
     elsif session[:user_id]
       user = User.find(session[:user_id])
     end
-    
+
     if user and user.settings.nil?
       user.create_settings
     end
   end
-  
+
   def find_session
     if session[:user_id]
       @user = User.find(session[:user_id])
