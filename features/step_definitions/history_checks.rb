058750901877b7434b672114d8f8327c8d4df3c5
Chris Howlett <chowlett09@gmail.com>
2017-05-02 17:07:16 +0100

    Update Smithy tests (card done previously)

--- features/step_definitions/history_checks.rb
+++ features/step_definitions/history_checks.rb
@@ -1,4 +1,4 @@
 Then /later history should include "(.*)"/ do |expected|
   expected.gsub!(/\[I\]/, 'Alan')
-  assert_contains @test_game.histories.where(['id > ?', @last_hist_id]).map(&:event), expected
+  assert_contains @test_game.journals.map(&:histories).flatten.select{ |h| h.created_at > @last_hist_time}.map(&:event), expected
 end
--- features/step_definitions/history_setup.rb
+++ features/step_definitions/history_setup.rb
@@ -1,3 +1,3 @@
 Given /I have noted the last history/ do
- @last_hist_id = @test_game.histories.last.id
+ @last_hist_time = @test_game.journals.map(&:histories).flatten.last.created_at
 end
