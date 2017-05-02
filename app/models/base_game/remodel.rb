a054abe46c05ce0fd94e4b13c9505f3276ef8b7e
Chris Howlett <chowlett09@gmail.com>
2017-05-02 16:44:37 +0100

    Convert Remodel to journaling

--- app/models/base_game/remodel.rb
+++ app/models/base_game/remodel.rb
@@ -4,82 +4,98 @@ class BaseGame::Remodel < Card
   card_text "Action (cost: 4) - Trash a card from your hand. " +
                        "Gain a card costing up to 2 more than the trashed card."
 
-  def play(parent_act)
+  TrashEventTempl = Journal::Template.new("{{player}} chose {{card}} to trash with #{readable_name}.")
+  TakeEventTempl = Journal::Template.new("{{player}} took {{supply_card}} with #{readable_name}.")
+
+  def play
     super
 
-    if player.cards(true).hand.map(&:class).uniq.length == 1
-      # Only holding one type of card. Call resolve_trash directly
-      return resolve_trash(player, {:card_index => 0}, parent_act)
-    elsif player.cards.hand.empty?
+    if player.cards.hand.empty?
       # Holding no cards. Just log
-      game.histories.create!(:event => "#{player.name} trashed nothing.",
+      game.add_history(:event => "#{player.name} trashed nothing.",
                             :css_class => "player#{player.seat} card_trash")
-    else
-      # Create a PendingAction to trash a card
-      act = parent_act.children.create!(:expected_action => "resolve_#{self.class}#{id}_trash",
-                                       :text => "Trash a card with Remodel",
-                                       :player => player,
-                                       :game => game)
+      return
     end
 
-    "OK"
+    if game.find_journal(TrashEventTempl).nil?
+      if player.cards.hand.map(&:class).uniq.length == 1
+        # Only holding one type of card. Pre-create the journal
+        game.add_journal(player_id: player.id,
+                          event: TrashEventTempl.fill(player: player.name, card: "#{player.cards.hand.first.readable_name} (0)"))
+      end
+    end
+
+    # Ask the required question.
+    game.ask_question(object: self, actor: player, method: :resolve_trash, text: "Trash a card with #{readable_name}.")
   end
 
-  def determine_controls(player, controls, substep, params)
-    case substep
-    when "trash"
+  def determine_controls(actor, controls, question)
+    case question.method
+    when :resolve_trash
       controls[:hand] += [{:type => :button,
-                          :action => :resolve,
                           :text => "Trash",
                           :nil_action => nil,
-                          :params => {:card => "#{self.class}#{id}",
-                                      :substep => "trash"},
-                          :cards => [true] * player.cards.hand.size
+                          journals: actor.cards.hand.each_with_index.map do |c, ix|
+                            TrashEventTempl.fill(player: actor.name, card: "#{c.readable_name} (#{ix})")
+                          end
                          }]
-    when "take"
-      valid_piles = game.piles.map do |pile|
-        (pile.cost <= (params[:trashed_cost].to_i + 2)) and not pile.empty?
-      end
+    when :resolve_take
       controls[:piles] += [{:type => :button,
-                            :action => :resolve,
-                            :text => "Take",
-                            :nil_action => nil,
-                            :params => {:card => "#{self.class}#{id}",
-                                        :substep => "take",
-                                        :trashed_cost => params[:trashed_cost]},
-                            :piles => valid_piles
-                          }]
+                           :text => "Take",
+                           :nil_action => nil,
+                           journals: game.piles.each_with_index.map do |pile, ix|
+                             if (pile.cost <= (question.params[:trashed_cost].to_i + 2) && !pile.cards.empty?)
+                               TakeEventTempl.fill(player: actor.name, supply_card: "#{pile.cards[0].readable_name} (#{ix})")
+                             end
+                           end
+                         }]
     end
   end
 
-  resolves(:trash).validating_params_has(:card_index).
-                   validating_param_is_card(:card_index, scope: :hand).
+  resolves(:trash).using(TrashEventTempl).
+                   validating_param_is_card(:card, scope: :hand).
                    with do
-    # Trash the selected card, and create a new PendingAction for picking up
+    # Trash the selected card, and create a new question for picking up
     # the remodelled card.
-    card = actor.cards.hand[params[:card_index].to_i]
-    card.trash
-    trashed_cost = card.cost
-    game.histories.create!(:event => "#{actor.name} trashed a #{card.class.readable_name} from hand (cost: #{trashed_cost}).",
-                          :css_class => "player#{actor.seat} card_trash")
+    journal.card.trash
+    trashed_cost = journal.card.cost
+    journal.add_history(:event => "#{actor.name} trashed a #{journal.card.readable_name} from hand (cost: #{trashed_cost}).",
+                        :css_class => "player#{actor.seat} card_trash")
 
-    act = parent_act.children.create!(:expected_action => "resolve_#{self.class}#{id}_take;trashed_cost=#{trashed_cost}",
-                                     :text => "Take a replacement card with Remodel",
-                                     :player => player,
-                                     :game => game)
+    candidates = game.piles.map.with_index do |pile, ix|
+      if (pile.cost <= (trashed_cost + 2) && !pile.cards.empty?)
+        [pile, ix]
+      else
+        nil
+      end
+    end.compact
 
-    "OK"
+    if candidates.length == 0
+      # Can't take a replacement. Just log.
+      game.add_history(:event => "#{player.name} couldn't take a card with #{readable_name}.",
+                        :css_class => "player#{player.seat} card_take")
+      return
+    end
+
+    if game.find_journal(TakeEventTempl).nil?
+      if candidates.length == 1
+        # Only one option. Fabricate the journal.
+        take_journal = game.add_journal(player_id: player.id,
+                                    event: TakeEventTempl.fill(player: player.name,
+                                     supply_card: "#{candidates[0][0].readable_name} (#{candidates[0][1]})"))
+      end
+    end
+
+    game.ask_question(object: self, actor: actor,
+                      method: :resolve_take,
+                      text: "Take a replacement card with #{readable_name}.",
+                      params: {trashed_cost: trashed_cost})
   end
 
-  resolves(:take).validating_params_has(:pile_index).
-                    validating_param_is_pile(:pile_index) { cost <= my{params}[:trashed_cost].to_i + 2 }.
-                    with do
+  resolves(:take).using(TakeEventTempl).
+                  validating_param_is_card(:supply_card, scope: :supply) { cost <= my{journal.params}[:trashed_cost].to_i + 2 }.
+                  with do
     # Process the take.
-    game.histories.create!(:event => "#{actor.name} took " +
-           "#{game.piles[params[:pile_index].to_i].card_class.readable_name} with Remodel.",
-                          :css_class => "player#{actor.seat} card_gain")
-    actor.gain(parent_act, :pile => game.piles[params[:pile_index].to_i])
-
-    "OK"
+    actor.gain(:card => journal.supply_card, journal: journal)
   end
 end
--- features/cards/base_game/remodel.feature
+++ features/cards/base_game/remodel.feature
@@ -20,9 +20,8 @@ Feature: Remodel
       And I should be able to choose the Copper, Silver, Estate, Chapel, Woodcutter, Throne Room piles
       And I should not be able to choose the Duchy, Gold, Mint, Adventurer piles
     When I choose the Throne Room pile
-      And the game checks actions
-    Then I should have gained Throne Room
-      And I should need to Play treasure
+      Then I should have gained Throne Room
+      And I should need to Play treasures
 
   Scenario: Playing Remodel - only one type in hand
     Given my hand contains Remodel, Estate x2
@@ -33,12 +32,11 @@ Feature: Remodel
       And I should be able to choose the Copper, Silver, Estate, Chapel, Woodcutter, Throne Room piles
       And I should not be able to choose the Duchy, Gold, Mint, Adventurer piles
     When I choose the Throne Room pile
-      And the game checks actions
-    Then I should have gained Throne Room
+      Then I should have gained Throne Room
       And it should be my Buy phase
 
   Scenario: Playing Remodel - nothing in hand
     Given my hand contains Remodel
       And it is my Play Action phase
     When I play Remodel
-    Then it should be my Play Treasure phase
+    Then it should be my Buy phase
