863df556017acdee025297ed504c662473a99833
Chris Howlett <chowlett09@gmail.com>
2017-05-02 15:05:30 +0100

    Convert Moneylender to journaling

--- app/models/base_game/moneylender.rb
+++ app/models/base_game/moneylender.rb
@@ -4,22 +4,20 @@ class BaseGame::Moneylender < Card
   card_text "Action (cost: 4) - Trash a Copper card from your hand. " +
                        "If you do, +3 cash."
 
-  def play(parent_act)
+  def play
     super
 
     card = player.cards.hand.of_type("BasicCards::Copper")[0]
     if card
       # Trash the selected card, and grant 3 cash.
       card.trash
-      game.histories.create!(:event => "#{player.name} trashed a Copper from hand.",
+      game.add_history(:event => "#{player.name} trashed a Copper from hand.",
                             :css_class => "player#{player.seat} card_trash")
 
-      player.add_cash(3)
+      player.cash += 3
     else
-      game.histories.create!(:event => "#{player.name} trashed nothing.",
+      game.add_history(:event => "#{player.name} trashed nothing.",
                             :css_class => "player#{player.seat} card_trash")
     end
-
-    "OK"
   end
 end
