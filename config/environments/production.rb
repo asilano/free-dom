963c869807db4543abf6f7c8c9596d0b145e2e39
crh54 <crh54@bath.ac.uk>
2017-09-05 17:01:06 +0100

    Changes for openshift -> dokku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Dominion::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'dom-inion.rhcloud.com' }
+  config.action_mailer.default_url_options = { :host => 'free-dom.nsict.org' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
