849cb54804fef442d2733a33ef724fd4f158752b
Chris Howlett <crh54@bath.ac.uk>
2018-04-05 12:14:06 +0100

    Temporary route in prod

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Temporary - so page "works" in production
+  get '/'                      => "rails/welcome#index"
 end
