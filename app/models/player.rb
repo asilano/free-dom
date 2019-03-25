368c1084a8ac35cb55f6a48548191ac74df7b3ff
Chris Howlett <crh54@bath.ac.uk>
2019-03-25 09:15:48 +0000

    Fix method of calling GROUP for counting

--- app/models/player.rb
+++ app/models/player.rb
@@ -1065,11 +1065,11 @@ class Player < ActiveRecord::Base
   end
 
   def cards_for_decklist(html = true)
-    deck = cards.unscoped.where(:player_id => self).count(:group => "type").sort do |gp_a,gp_b|
+    deck = cards.unscope(:order).group(:type).count.sort do |gp_a, gp_b|
       type_a = gp_a[0]
       type_b = gp_b[0]
-      a = cards.find_by_type(type_a)
-      b = cards.find_by_type(type_b)
+      a = cards.find_by(type: type_a)
+      b = cards.find_by(type: type_b)
 
       if type_a == type_b
         0
