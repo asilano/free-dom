5b152f9587d99267c8cb0196faafce2055b4db20
Chris Howlett <crh54@bath.ac.uk>
2021-03-02 16:51:04 +0000

    GNU Terry Pratchett
    For #167

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :add_clacks_overhead
 
   def index
   end
 
+  private
+
+  def add_clacks_overhead
+    response.set_header('X-Clacks-Overhead', 'GNU Terry Pratchett')
+  end
 end
