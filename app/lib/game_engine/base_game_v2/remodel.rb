4ac859b3076ebaa7a9abcd1134551a4bc2fcf436
Chris Howlett <crh54@bath.ac.uk>
2021-04-08 11:54:34 +0100

    Factor out trash-from-hand journal

--- app/lib/game_engine/base_game_v2/remodel.rb
+++ app/lib/game_engine/base_game_v2/remodel.rb
@@ -12,46 +12,12 @@ module GameEngine
         game_state.get_journal(TrashCardJournal, from: played_by).process(game_state)
       end
 
-      class TrashCardJournal < Journal
-        define_question('Choose a card to trash').with_controls do |_game_state|
-          [OneCardControl.new(journal_type: TrashCardJournal,
-                              question:     self,
-                              player:       @player,
-                              scope:        :hand,
-                              text:         'Trash',
-                              null_choice:  if @player.hand_cards.empty?
-                                              { text: 'Trash nothing', value: 'none' }
-                                            end,
-                              css_class:    'trash-card')]
-        end
+      class TrashCardJournal < CommonJournals::TrashJournal
+        configure question_text: 'Choose a card to trash'
 
-        validation do
-          return false if journal.params['choice'] == 'none' && @player.hand_cards.present?
-          return true if journal.params['choice'] == 'none'
-          return false unless journal.params['choice']&.integer?
-
-          journal.params['choice'].to_i < journal.player.hand_cards.length
-        end
-
-        process do |_game_state|
-          if params['choice'] == 'none'
-            @histories << History.new("#{player.name} trashed nothing.",
-                                      player:      player,
-                                      css_classes: %w[trash-card])
-            return
-          end
-
-          # Trash the chosen card on its owner's deck
-          card = player.hand_cards[params['choice'].to_i]
-          card_cost = card.cost
-          @histories << History.new("#{player.name} trashed #{card.readable_name} from their hand.",
-                                    player:      player,
-                                    css_classes: %w[trash-card])
-          card.trash(from: player.cards)
-          observe
-
+        def post_process
           # Ask the player to take a replacement
-          game_state.get_journal(GainCardJournal, from: player, opts: { trashed_cost: card_cost }).process(game_state)
+          game_state.get_journal(GainCardJournal, from: player, opts: { trashed_cost: @card_cost }).process(game_state)
         end
       end
 
---
+++ app/models/common_journals/trash_journal.rb
@@ -0,0 +1,48 @@
+module CommonJournals
+  class TrashJournal < Journal
+    def self.configure(question_text: nil, question_block: nil, filter: nil)
+      define_question(question_text, &question_block).with_controls do |_game_state|
+        [OneCardControl.new(journal_type: self,
+                            question:     self,
+                            player:       @player,
+                            scope:        :hand,
+                            text:         'Trash',
+                            filter:       filter,
+                            # If the card to trash has conditions, it's legal to trash
+                            # nothing, to avoid trust issues.
+                            null_choice:  if @player.hand_cards.empty? || filter
+                                            { text: 'Trash nothing', value: 'none' }
+                                          end,
+                            css_class:    'trash-card')]
+      end
+    end
+
+    validation do
+      return false if journal.params['choice'] == 'none' && @player.hand_cards.present?
+      return true if journal.params['choice'] == 'none'
+      return false unless journal.params['choice']&.integer?
+
+      journal.params['choice'].to_i < journal.player.hand_cards.length
+    end
+
+    process do |_game_state|
+      if params['choice'] == 'none'
+        @histories << GameEngine::History.new("#{player.name} trashed nothing.",
+                                              player:      player,
+                                              css_classes: %w[trash-card])
+        return
+      end
+
+      # Trash the chosen card from its owner's hand
+      card = player.hand_cards[params['choice'].to_i]
+      @card_cost = card.cost
+      @histories << GameEngine::History.new("#{player.name} trashed #{card.readable_name} from their hand.",
+                                            player:      player,
+                                            css_classes: %w[trash-card])
+      card.trash(from: player.cards)
+      observe
+
+      post_process if respond_to?(:post_process)
+    end
+  end
+end
