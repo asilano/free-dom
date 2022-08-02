43691b2ea32e8799e4a1f7b167ee0bfecf53dd0c
Chris Howlett <chris.howlett@freeagent.com>
2022-08-02 15:40:13 +0100

    Convert Play Treasures to a multi-card
    This is strictly counter to the rules, but is an important UI streamline
    concession. Default behaviour of the Play Treasures journal is now to
    play all treasures in order, left to right. An extra no-action button is
    added to stop playing treasures.
    
    Framework and tests are updated to handle:
    
    * multiple no-card buttons on a control
    * a null_card button on MultiCardControl

--- app/lib/multi_card_control.rb
+++ app/lib/multi_card_control.rb
@@ -4,11 +4,17 @@ class MultiCardControl < Control
   def initialize(opts = {})
     super
     @filter = filter_from(opts[:filter]) || ->(_card) { true }
-    @preselect = opts[:preselect] || ->(_card) { false }
+    @preselect = filter_from(opts[:preselect]) || ->(_card) { false }
     if opts.key? :submit_text
-      @cardless_button = { text: opts[:submit_text],
-                           value: '',
-                           key: 'submit' }
+      add_cardless_button({ text: opts[:submit_text],
+                            value: "",
+                            key: "submit" })
+    end
+
+    if opts.key? :null_choice
+      add_cardless_button({ text:  opts[:null_choice][:text],
+                            value: opts[:null_choice][:value],
+                            key: "choice" })
     end
   end
 end
