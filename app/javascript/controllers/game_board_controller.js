f786f8e5df69572db8f7806ec225a1d275e53ad4
Chris Howlett <chris.howlett@freeagent.com>
2022-09-24 21:34:51 +0100

    Change page waiting-for title by Stimulus

--- app/javascript/controllers/game_board_controller.js
+++ app/javascript/controllers/game_board_controller.js
@@ -1,6 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static values = { title: String }
+
   connect() {
+    document.title = this.titleValue
   }
 }
