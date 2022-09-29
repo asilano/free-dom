fa5ea2151373cfdaada0b025d607a46bb48ea7a1
Chris Howlett <chris.howlett@freeagent.com>
2022-09-29 20:01:17 +0100

    Scroll log from Stimulus hook

--- app/javascript/controllers/game_board_controller.js
+++ app/javascript/controllers/game_board_controller.js
@@ -2,8 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static values = { title: String }
+  static targets = ["journals"]
 
   connect() {
     document.title = this.titleValue
+    this.journalsTarget.scrollTop = this.journalsTarget.scrollHeight
   }
 }
--- app/javascript/src/games.js
+++ app/javascript/src/games.js
@@ -14,24 +14,6 @@ onmount('.random-regenerate', function() {
   $(this).on('ajax:success', event => $('.random-name').text(event.detail[0]));
 });
 
-let scrollPosition = 0;
-const refreshTimer = undefined;
-onmount('body[data-controller=games][data-action=show]', function() {
-  let reloader;
-  clearTimeout(refreshTimer);
-  $('html').scrollTop(scrollPosition);
-  scrollPosition = 0;
-
-  const journals = $('#journal-log')[0];
-  journals.scrollTop = journals.scrollHeight;
-  const SECONDS = 30;
-  reloader = function() {
-    scrollPosition = $('html').scrollTop();
-    Turbo.visit(location.toString(), {action: 'replace'});
-  };
-});
-  // refreshTimer = setTimeout reloader, SECONDS * 1000
-
 onmount('#discord-report', function() {
   $(this).on('click', 'span', function(e) {
     $(this).siblings('form').toggle();
