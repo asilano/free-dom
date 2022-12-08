e67c6babad280da7de5a4827a4b7ef461c4793ad
Chris Howlett <chris.howlett@freeagent.com>
2022-12-08 22:09:44 +0000

    Turn asset digests back on

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # Enable asset digests
+  config.assets.digest = true
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
